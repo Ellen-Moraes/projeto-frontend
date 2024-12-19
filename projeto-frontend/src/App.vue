759b1b2662aff817a632b277c77283d6ce838c48
suell <elleven549@gmail.com>
2024-12-19 15:38:55 -0300

    Inicial commit do projeto

---
+++ projeto-frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="container">
+    <MyComponent />
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ projeto-frontend/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-component">
+    <h1 class="text-center">Olá Vue.js com Bootstrap!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent'
+}
+</script>
+
+<style>
+.my-component {
+  background-color: #f8f9fa;
+  padding: 20px;
+  border-radius: 5px;
+}
+</style>
